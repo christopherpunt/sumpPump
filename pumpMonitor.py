d464f7f4b8999f5351b72e45bba53201f00dee30
christopherpunt <chrispunt13@icloud.com>
2020-06-04 01:47:14 -0400

    working pump monitor and remoteMonitor

--- pumpMonitor.py
+++ pumpMonitor.py
@@ -1,6 +1,7 @@
 import time
 import paho.mqtt.client as mqtt
 import RPi.GPIO as GPIO
+import threading
 import send_emails
 
 BROKER = 'iot.cs.calvin.edu'
@@ -11,7 +12,16 @@ CERTS = '/etc/ssl/certs/ca-certificates.crt'
 PORT = 8883
 QOS = 0
 
-MAIN_THREAD_DELAY = 5
+MAIN_THREAD_DELAY = 10
+TIMERLEN = 5
+global t
+
+def timeout():
+    print("time expired")
+    client.publish(TOPIC, "connected")    
+
+t = threading.Timer(TIMERLEN, timeout)
+t.start()
 
 operational = 1
 powerOutage = 2
@@ -24,7 +34,7 @@ stateList = [operational, powerOutage, internetOutage]
 FLOATLOW = 21
 FLOATMID = 20
 FLOATHIGH = 16
-FLOATOVER = 26
+FLOATOVER = 19
 glitchFilter = 100
 
 # Setup GPIO mode
@@ -46,6 +56,10 @@ def on_connect(client, userdata, flags, rc):
 # Callback when a message is published
 def on_publish(client, userdata, mid):
     print("MQTT data published")
+    global t
+    t.cancel()
+    t = threading.Timer(TIMERLEN, timeout)
+    t.start()
 
 # Callback when client receives a PUBLISH message from the broker
 def on_message(client, data, msg):
@@ -59,29 +73,37 @@ client.username_pw_set(USERNAME, password=PASSWORD)
 client.tls_set(CERTS)
 client.on_connect = on_connect
 client.on_message = on_message
+client.on_publish = on_publish
 
 # Connect to MQTT broker and subscribe to the button topic
 client.connect(BROKER, PORT, 60)
 client.subscribe(TOPIC, qos=QOS)
 client.loop_start()
 
-def levelListener(channel):
+def levelListener(channel):    
     if channel == FLOATLOW:
+        client.publish(TOPIC, "low")
         print("FLOATLOW")
     elif channel == FLOATMID:
+        client.publish(TOPIC, "mid")
         print("FLOATMID")
     elif channel == FLOATHIGH:
+        client.publish(TOPIC, "high")
         print("FLOATHIGH")
     elif channel == FLOATOVER:
+        client.publish(TOPIC, "over")
         print("****the sump pump is overflowing!****")
+        #TODO: send email
 
-GPIO.add_event_detect(FLOATLOW, GPIO.RISING, callback=levelListener, bouncetime=100)
-GPIO.add_event_detect(FLOATMID, GPIO.RISING, callback=levelListener, bouncetime=100)
-GPIO.add_event_detect(FLOATHIGH, GPIO.RISING, callback=levelListener, bouncetime=100)
-GPIO.add_event_detect(FLOATOVER, GPIO.RISING, callback=levelListener, bouncetime=100)
+#event detection for float switches
+GPIO.add_event_detect(FLOATLOW, GPIO.FALLING, callback=levelListener, bouncetime=500)
+GPIO.add_event_detect(FLOATMID, GPIO.FALLING, callback=levelListener, bouncetime=500)
+GPIO.add_event_detect(FLOATHIGH, GPIO.FALLING, callback=levelListener, bouncetime=500)
+GPIO.add_event_detect(FLOATOVER, GPIO.FALLING, callback=levelListener, bouncetime=500)
 
 try:
     while True:
+
         time.sleep(MAIN_THREAD_DELAY)
 
 except KeyboardInterrupt:
--- remoteMonitor.py
+++ remoteMonitor.py
@@ -1,6 +1,7 @@
 import time
 import paho.mqtt.client as mqtt
-# import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
+import threading
 
 BROKER = 'iot.cs.calvin.edu'
 USERNAME = "cs300" # Put broker username here
@@ -10,30 +11,112 @@ CERTS = '/etc/ssl/certs/ca-certificates.crt'
 PORT = 8883
 QOS = 0
 
-MAIN_THREAD_DELAY = 10
+MAIN_THREAD_DELAY = 5
+TIMERLEN = 6
 
-operational = 1
-powerOutage = 2
-internetOutage = 3
+NORMAL = 1
+OVERFLOWING = 2
+POWEROUTAGE = 3
+INTERNETOUTAGE = 4
 
-state = operational
-stateList = [operational, powerOutage, internetOutage]
+
+state = NORMAL
+stateList = [NORMAL, OVERFLOWING, INTERNETOUTAGE]
+
+LEDLOW = 17
+LEDMID = 27
+LEDHIGH = 22
+LEDOVER = 23
+LEDs = [LEDLOW,LEDMID,LEDHIGH,LEDOVER]
+
+GPIO.setmode(GPIO.BCM) # Use BCM numbers
+GPIO.setup(LEDLOW, GPIO.OUT)
+GPIO.setup(LEDMID, GPIO.OUT)
+GPIO.setup(LEDHIGH, GPIO.OUT)
+GPIO.setup(LEDOVER, GPIO.OUT)
+
+global t
+
+def timeout():
+    state = INTERNETOUTAGE
+    print("time expired")
+    #sendEmail
+    global t
+    t.cancel()
+
+t = threading.Timer(TIMERLEN, timeout)
+t.start()
+
+def turnOneLEDOn(myled):
+    global LEDs
+    for led in LEDs:
+        GPIO.output(led, False)
+    GPIO.output(myled, True)
+
+turnOneLEDOn(LEDLOW)
+
+# Callback when connecting to the MQTT broker
+def on_connect(client, userdata, flags, rc):
+    if rc==0:
+        print('Connected to',BROKER)
+    else:
+        print('Connection to',BROKER,'failed. Return code=',rc)
+        exit(1)
 
 
 # Callback when client receives a PUBLISH message from the broker
 def on_message(client, data, msg):
+    global t
+    global state
+    normalList = ["low", "mid", "high"]
     if msg.topic == TOPIC:
-        message = int(msg.payload)
-        print("Received message: LED = ", message)
+        message = msg.payload.decode("utf-8")
+        #if there was an internet outage
+        if state == INTERNETOUTAGE:
+            if message in normalList:
+                state = NORMAL
+            elif message == "over":
+                state = OVERFLOWING
+            #send email saying reconnected
 
-        if message == operational:
-            pass
-            #set lights
-        elif message == powerOutage:
-            pass
-            #set lights
-        else:
-            # sendEmail()
+        else: #state != INTERNETOUTAGE
+            if message in normalList:
+                state = NORMAL
+                #reset timer            
+                t.cancel()
+                t = threading.Timer(TIMERLEN, timeout)
+                t.start()
+                print()
+                
+                if message == "low":
+                    turnOneLEDOn(LEDLOW)
+                    print("NORMAL: low")
+                elif message == "mid":
+                    turnOneLEDOn(LEDMID)
+                    print("NORMAL: mid")
+                elif message == "high":
+                    turnOneLEDOn(LEDHIGH)
+                    print("NORMAL: high")
+                else:
+                    turnOneLEDOn(LEDOVER)
+                    print("ERROR! I don't know what happened")
+
+            elif message == "over":
+                state = OVERFLOWING
+                #reset timer
+                t.cancel()
+                t = threading.Timer(TIMERLEN, timeout)
+                t.start()
+                turnOneLEDOn(LEDOVER)
+                #sendEmail()
+                print("the pump is overflowing LEAVE NOW, sending email")
+            elif message == "connected":
+                t.cancel()
+                t = threading.Timer(TIMERLEN, timeout)
+                t.start()
+                print("still connected")
+            else:
+                print("Error: unrecognized message = " + message)
 
         
 # Setup MQTT client and callbacks
@@ -49,3 +132,12 @@ client.subscribe(TOPIC, qos=QOS)
 client.loop_start()
 
 
+try:
+    while True:
+        time.sleep(MAIN_THREAD_DELAY)
+        
+
+except KeyboardInterrupt:
+    print("\nDone")
+    GPIO.cleanup()
+    client.disconnect()
